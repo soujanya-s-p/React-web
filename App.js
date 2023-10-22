35138a9d0f340988c1e05b8a9084af0f81925f30
soujanya-s-p <101053024+soujanya-s-p@users.noreply.github.com>
2023-10-22 21:58:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import './styles/App.css';  Make sure to adjust the path if necessary
+import Frame1 from './frames/Frame1/Frame1.jsx';
+import Frame2 from './frames/Frame2/Frame2.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Frame1 />
+      <Frame2 />
+    </div>
+  );
+}
+
+export default App;
